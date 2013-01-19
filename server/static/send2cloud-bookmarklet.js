009ad431331672dfd4e27875a6fbe8c18b511adb
Jeff Jia <jeffjia@outlook.com>
2013-01-18 22:55:57 -0800

    add license file, and add bookmarklet for page.

---
+++ server/static/send2cloud-bookmarklet.js
@@ -0,0 +1,11 @@
+javascript:(function()%20%7B%20var%20s%20=%20document.createElement(%22script%22);%20s.setAttribute(%22id%22,%20%22bitmark_js%22);%20s.setAttribute(%22type%22,%20%22text/javascript%22);%20s.setAttribute(%22src%22,%20%22//bitly.com/a/bitmarklet.js%22);%20(top.document.body%20%7C%7C%20top.document.getElementsByTagName(%22head%22)%5B0%5D).appendChild(s);%20%7D)();
+
+(function() {
+  var s = document.createElement('script');
+  s.setAttribute('id', 'send2cloud_js');
+  s.setAttribute('type', 'text/javascript');
+  s.setAttribute('src', 'http://send2cloud-server.appspot.com/static/bookmarklet.js');
+  (top.document.body || top.document.getElementByTagName('head')[0]).appendChild(s);
+})();
+
+javascript:(function()%20%7Bvar%20s%20=%20document.createElement(%22script%22);s.setAttribute(%22id%22,%20%22send2cloud_js%22);s.setAttribute(%22type%22,%20%22text/javascript%22);s.setAttribute(%22src%22, %22http%3A//send2cloud-server.appspot.com/static/bookmarklet.js%22);(top.document.body%20%7C%7C%20top.document.getElementByTagName(%22head%22)%5B0%5D).appendChild(s);%7D)();
