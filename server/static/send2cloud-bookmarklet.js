9509f0005b0505e48a53144b33c5314003804b43
Jeff Jia <jeffjia@outlook.com>
2013-01-19 22:30:13 -0800

    modify bookmarklet to make life much easier.

--- server/static/send2cloud-bookmarklet.js
+++
@@ -1,7 +0,0 @@
-(function() {
-  var s = document.createElement("script");
-  s.setAttribute("id", "send2cloud_js");
-  s.setAttribute("type", "text/javascript");
-  s.setAttribute("src", "http://send2cloud-server.appspot.com/static/bookmarklet.js");
-  (top.document.body || top.document.getElementByTagName("head")[0]).appendChild(s);
-})();
