58312bdd880185e21c12c1338ad0481de287d606
Jeff Jia <jeffjia@outlook.com>
2013-01-24 19:51:24 -0800

    modify UI a bit

--- server/static/bookmarklet.js
+++ server/static/bookmarklet.js
@@ -32,13 +32,11 @@
           $("body").append("\
             <div id='s2cframe'>\
               <div id='s2cframe_veil' style=''>\
-                <p>Loading...</p>\
               </div>\
               <iframe src='//localhost:8080/send?u=" + u + "' onload=\"$('#s2cframe iframe').slideDown(500);\">Enable iFrames.</iframe>\
               <style type='text/css'>\
-                #s2cframe_veil { display: none; position: fixed; width: 100%; height: 100%; top: 0; left: 0; background-color: rgba(255,255,255,.75); cursor: pointer; z-index: 900; }\
-                #s2cframe_veil p { color: black; font: normal normal bold 20px/20px Helvetica, sans-serif; position: absolute; top: 50%; left: 50%; width: 10em; margin: -10px auto 0 -5em; text-align: center; }\
-                #s2cframe iframe { display: none; position: fixed; top: 10%; left: 10%; width: 80%; height: 80%; z-index: 999; border: 10px solid rgba(0,0,0,.5); margin= -5px 0 0 -5px; }\
+                #s2cframe_veil { display: none; position: fixed; width: 100%; height: 100%; top: 0; left: 0; background-color: rgba(255,255,255,.25); cursor: pointer; z-index: 900; }\
+                #s2cframe iframe { display: none; position: fixed; top: 10%; left: 30%; width: 40%; height: 30%; z-index: 999; border: 10px solid rgba(0,0,0,.5); margin= -5px 0 0 -5px; }\
               </style>\
             </div>");
             $("#s2cframe_veil").fadeIn(750);
